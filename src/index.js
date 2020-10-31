ecd621993e53a11fff0c5b9bbcc5c8b2fb05e6e6
BURHANUDDIN <Bmerchant945@gmail.com>
2020-10-31 22:13:38 +0530

    added the cors dependency

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = new express();
 
 const port = process.env.PORT;
 const cors = require("cors");
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 const Admin = require("./routers/Admin");
 const Blog = require("./routers/Blog");
