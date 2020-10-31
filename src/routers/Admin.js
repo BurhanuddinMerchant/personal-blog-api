cfce95466d2f00b0219fc3c6ba6706952293f0ba
BURHANUDDIN <Bmerchant945@gmail.com>
2020-10-31 21:55:23 +0530

    changed import for auth middleware

--- src/routers/Admin.js
+++ src/routers/Admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const Admin = require("../models/Admin");
-const auth = require("../middleware/authentication");
+const auth = require("../middleware/Authentication");
 
 // admin login path
 router.get("/admin/login", async (req, res) => {
